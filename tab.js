e3c06988fd2145ff8af4bd9fd05c2b77aac4e209
Song4025 <thdgus4025@gmail.com>
2022-05-19 02:11:02 +0900

    add bubblingSolution and for, dataset

--- tab.js
+++ tab.js
@@ -33,7 +33,7 @@ button[2].addEventListener('click', ()=>{
     jContent.removeClass('show');
     content[2].classList.add('show');
 });
-*/
+
 
 // 근데 위 코드가 계속 반복되네? 그럼 반복문을 이용해서 코드를 줄여보자.
 for(let i = 0; i < jButton.length; i++){
@@ -45,3 +45,62 @@ for(let i = 0; i < jButton.length; i++){
     });
 }
 
+// 재사용성을 위해 함수로 만들어버리자.
+for(let i = 0; i < jButton.length; i++){
+    button[i].addEventListener('click', ()=>{
+        openTab(i);
+    });
+}
+
+function openTab(p) {
+    jButton.removeClass('orange');
+    button[p].classList.add('orange');
+    jContent.removeClass('show');
+    content[p].classList.add('show');
+}
+
+
+// addEventListener를 1개만 쓰게 만들어보자. (이벤트리스너를 사용하면 램을 쓰기때문이다.)
+$('.list').click(function(e){
+    // if (e.target == document.querySelectorAll('.tab-button')[0] ){
+    //     openTab(0);
+    // }else if(e.target == document.querySelectorAll('.tab-button')[1] ){
+    //     openTab(1);
+    // }else if(e.target == document.querySelectorAll('.tab-button')[2] ){
+    //     openTab(2);
+    // }else if(e.target == document.querySelectorAll('.tab-button')[3] ){
+    //     openTab(3);
+    // }else if(e.target == document.querySelectorAll('.tab-button')[4] ){
+    //     openTab(4);
+    // }
+    for(let i = 0; i < jButton.length; i++){
+        if (e.target == document.querySelectorAll('.tab-button')[i]){
+            openTab(i);
+        }
+    }
+});
+
+function openTab(p) {
+    jButton.removeClass('orange');
+    button[p].classList.add('orange');
+    jContent.removeClass('show');
+    content[p].classList.add('show');
+}
+*/
+
+// 문법 : html에 data-작명="값" 을 입력하면 사용자는 볼 수 없는 숨겨진 값을 넣을 수 있음.
+// 브라우저콘솔 에서 document.querySelector('.tab-button').dataset.id 하면 찾을 수 있음
+$('.list').click(function(e){
+    // 지금누른게 버튼 0이면 버튼 0에 orange클래스명 추가 거시기 어쩌구
+    // 그런데 만약 data-작명="값"의 문법을 알고있다면?
+    // 함수(지금누른 버튼에 숨겨져있는 data-작명);
+    openTab(e.target.dataset.id);
+    console.log(parseInt(e.target.dataset.id));
+});
+
+function openTab(p) {
+    jButton.removeClass('orange');
+    button[p].classList.add('orange');
+    jContent.removeClass('show');
+    content[p].classList.add('show');
+}
